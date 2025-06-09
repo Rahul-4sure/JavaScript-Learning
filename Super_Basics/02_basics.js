31d429904c3640aa2199d5c04b24fd832e13ecd0
Rahul <143538094+Rahul-4sure@users.noreply.github.com>
2025-06-09 11:29:46 +0000

    DataTypes

---
+++ Super_Basics/02_basics.js
@@ -0,0 +1,17 @@
+//DataTypes
+/*
+1.String => "Rahul"
+2.number => 123
+3.boolean => 0,1 or True,False
+4.BigInt
+5.Null
+6.Undefined
+
+
+=> Objects
+
+*/
+
+let name  = "Rahul";
+
+console.log(typeof name);
