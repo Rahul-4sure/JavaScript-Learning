458144960ca8b81820aef5f040ae2dbdaf35118c
Rahul <143538094+Rahul-4sure@users.noreply.github.com>
2025-06-09 11:42:00 +0000

    TypeConversion

---
+++ Super_Basics/03_TypeConversion.js
@@ -0,0 +1,25 @@
+let a = 33
+let b = "35"
+let c = true
+let d = null
+let e = undefined
+
+//to convert => number(variable)
+
+let con = Number(b)
+let f = Number(c)
+let n = Number(d)
+
+console.table([con,f,n])
+
+
+/* 
+
+'33' => 33
+"33asv" => NaN
+
+number => number()
+String => String()
+Bool => Boolean()
+etc
+*/
